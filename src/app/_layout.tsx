9849334ffdc663e3f9f6db2983d77f481a8254cc
dmitry <dmitryneznanov31@gmail.com>
2026-03-25 20:28:40 +0500

    add nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+import { Text, View } from "react-native"
+import "../global.css"
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+    <View>
+      <Text className=" text-pink-800 font-black">Tailwind test test</Text>
+    </View>
+  )
 }
